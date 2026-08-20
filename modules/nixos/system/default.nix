644ab9764e71056eb0f9adb15c931c25de44eeb5
sudo-mac <sudo-mac@users.noreply.github.com>
2026-08-20 04:47:56 -0400

    add godot to packages

--- modules/nixos/system/default.nix
+++ modules/nixos/system/default.nix
@@ -1,138 +1,139 @@
 {
-  inputs,
-  pkgs,
-  ...
+    inputs,
+    pkgs,
+    ...
 }:
 {
-  imports = [
-    ./users.nix
-    # ./bitwarden.nix
-    ./nvidia.nix
-    ./time-and-date.nix
-    ./nextcloud.nix
-    ./audio.nix
-    ./graphics.nix
-    ./gaming.nix
-    ./minecraft
-    ./roblox.nix
-    ./asus.nix
-    ./bootloader.nix
-    ./virtualisation.nix
-    ./libreoffice.nix
-    ./qflipper.nix
-    ./screen.nix
-    ./yazi.nix
-    ./rustdesk.nix
-    ./rustdesk-secrets.nix
-    ./pokemmo.nix
-    ./hyprland
-  ];
+    imports = [
+        ./users.nix
+        # ./bitwarden.nix
+        ./nvidia.nix
+        ./time-and-date.nix
+        ./nextcloud.nix
+        ./audio.nix
+        ./graphics.nix
+        ./gaming.nix
+        ./minecraft
+        ./roblox.nix
+        ./asus.nix
+        ./bootloader.nix
+        ./virtualisation.nix
+        ./libreoffice.nix
+        ./qflipper.nix
+        ./screen.nix
+        ./yazi.nix
+        ./rustdesk.nix
+        ./rustdesk-secrets.nix
+        ./pokemmo.nix
+        ./hyprland
+    ];
 
-  # Enabling Flakes
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
+    # Enabling Flakes
+    nix.settings.experimental-features = [
+        "nix-command"
+        "flakes"
+    ];
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
+    # Enable CUPS to print documents.
+    services.printing.enable = true;
 
-  # Enable Razer peripherals
-  hardware.openrazer.enable = true;
+    # Enable Razer peripherals
+    hardware.openrazer.enable = true;
 
-  # Enable USBmuxd
-  services.usbmuxd.enable = true;
+    # Enable USBmuxd
+    services.usbmuxd.enable = true;
 
-  # Enable networking
-  networking.networkmanager = {
-    enable = true;
-  };
-
-  # Enable Bluetooth
-  services.blueman.enable = true;
-
-  hardware.bluetooth = {
-    enable = true;
-    powerOnBoot = true;
-    settings = {
-      General = {
-        AutoEnable = true;
-        FastConnectable = true;
-      };
+    # Enable networking
+    networking.networkmanager = {
+        enable = true;
     };
-  };
 
-  # Enable Flatpak
-  services.flatpak.enable = true;
+    # Enable Bluetooth
+    services.blueman.enable = true;
 
-  #Enable Zsh System-wide
-  programs.zsh.enable = true;
+    hardware.bluetooth = {
+        enable = true;
+        powerOnBoot = true;
+        settings = {
+            General = {
+                AutoEnable = true;
+                FastConnectable = true;
+            };
+        };
+    };
 
-  # Force Plasma SSh Keypass Instead of GNOME
-  programs.ssh.askPassword = "";
+    # Enable Flatpak
+    services.flatpak.enable = true;
 
-  services.gnome.gcr-ssh-agent.enable = false;
+    #Enable Zsh System-wide
+    programs.zsh.enable = true;
 
-  services.logind.settings.Login = {
-    HandleLidSwitch = "ignore";
-    HandleLidSwitchDocked = "ignore";
-  };
+    # Force Plasma SSh Keypass Instead of GNOME
+    programs.ssh.askPassword = "";
 
-  hardware.sensor.iio.enable = true;
+    services.gnome.gcr-ssh-agent.enable = false;
 
-  environment.systemPackages = with pkgs; [
-    appimage-run
-    bat
-    brightnessctl
-    btop
-    curl
-    exfatprogs
-    fastfetch
-    freecad
-    gimp
-    git
-    git-lfs
-    gparted
-    grim
-    gzip
-    inkscape
-    inputs.iio-hyprland.packages.${pkgs.stdenv.hostPlatform.system}.default
-    jq
-    kdePackages.ark
-    kdePackages.dolphin
-    libimobiledevice
-    lolcat
-    networkmanagerapplet
-    nh
-    nix-output-monitor
-    onefetch
-    openrazer-daemon
-    orca-slicer
-    p7zip
-    polychromatic
-    razer-cli
-    ripgrep
-    scrcpy
-    slurp
-    superfile
-    tigervnc
-    tree
-    ungoogled-chromium
-    unzip
-    weathr
-    wget
-    zip
+    services.logind.settings.Login = {
+        HandleLidSwitch = "ignore";
+        HandleLidSwitchDocked = "ignore";
+    };
 
-    # Wine Packages
-    winePackages.full
-    winetricks
-  ];
+    hardware.sensor.iio.enable = true;
 
-  boot.kernelModules = [
-    "usbnet"
-    "cdc_ether"
-  ];
+    environment.systemPackages = with pkgs; [
+        appimage-run
+        bat
+        brightnessctl
+        btop
+        curl
+        exfatprogs
+        fastfetch
+        freecad
+        gimp
+        git
+        git-lfs
+        godot
+        gparted
+        grim
+        gzip
+        inkscape
+        inputs.iio-hyprland.packages.${pkgs.stdenv.hostPlatform.system}.default
+        jq
+        kdePackages.ark
+        kdePackages.dolphin
+        libimobiledevice
+        lolcat
+        networkmanagerapplet
+        nh
+        nix-output-monitor
+        onefetch
+        openrazer-daemon
+        orca-slicer
+        p7zip
+        polychromatic
+        razer-cli
+        ripgrep
+        scrcpy
+        slurp
+        superfile
+        tigervnc
+        tree
+        ungoogled-chromium
+        unzip
+        weathr
+        wget
+        zip
 
-  # NixOS Version
-  system.stateVersion = "26.11"; # Did you read the comment?
+        # Wine Packages
+        winePackages.full
+        winetricks
+    ];
+
+    boot.kernelModules = [
+        "usbnet"
+        "cdc_ether"
+    ];
+
+    # NixOS Version
+    system.stateVersion = "26.11"; # Did you read the comment?
 }
